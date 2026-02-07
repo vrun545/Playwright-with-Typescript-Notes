64cbb81610550411ace553b53aa9fa97b286089a
vrun545 <vrun545@gmail.com>
2026-02-08 01:15:09 +0530

    Added date Picker test case

--- my-ui-tests/datePicker.spec.ts
+++ my-ui-tests/datePicker.spec.ts
@@ -1,7 +1,34 @@
 import {test, expect, Locator, Browser} from "@playwright/test";
 
+test("handle custome dates in Date picker", async({page}) => {
 
-test("Automating Date Picker", async({page}) => {
+    await page.goto("https://testautomationpractice.blogspot.com/");
+
+    const year: string = '2021';
+    const month: string = 'March';
+    const date: string = "20";
+
+    await page.click("#datepicker");
+
+    while(true) {
+
+        const currentYear: string | null = await page.locator(".ui-datepicker-year").textContent();
+        const currentMonth: string | null = await page.locator(".ui-datepicker-month").textContent();
+
+        if(currentMonth === month && currentYear === year){
+            break;
+        }
+        await page.locator("[title='Prev']").click();
+    }
+
+    // date selection - without loop
+    await page.click(`//a[@class='ui-state-default'][text()='${date}']`);
+
+    await page.waitForTimeout(4000);
+
+});
+
+test("Date Picker sending values directly", async({page}) => {
 
    await page.goto("https://jqueryui.com/datepicker/"); 
 
@@ -14,11 +41,7 @@ test("Automating Date Picker", async({page}) => {
 
    // selecting today's date   
    const todayDate: Locator = frameElement.locator(".ui-datepicker-today");
-   await todayDate.click();
-
-   // custom date value
-   const customDate: Locator = frameElement.locator(".ui-datepicker-today > a");
-   await customDate.click();    
+   await todayDate.click();  
 
    await page.waitForTimeout(2000);
 
