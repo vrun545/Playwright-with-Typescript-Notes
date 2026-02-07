21b492ce8f0da8b37090150fba5541f51055df8b
vrun545 <vrun545@gmail.com>
2026-02-08 01:57:40 +0530

    Added Mock API Tests

---
+++ api-tests/mock_API_Request.spec.ts
@@ -0,0 +1,27 @@
+import {test, expect} from "@playwright/test";
+
+test("Mock API request in Playwright", async ({page}) => {
+
+    // Mock API Request
+    await page.route("*/**/api/v1/fruits", async route => {
+        const json = [
+            { name: "Typescript", id: 12 },
+            { name: "Javascript", id: 13 },
+            { name: "Java", id: 14 },
+            { name: "Python", id: 15 },
+            { name: "C#", id: 16 },
+        ];
+        await route.fulfill({json});
+
+    })
+
+    // Go to URL
+    await page.goto("https://demo.playwright.dev/api-mocking/");
+
+    // Assertions
+    await expect(page.getByText("Typescript")).toBeVisible();
+    await expect(page.getByText("C#")).toBeVisible();
+    await expect(page.getByText("Python")).toBeVisible();
+    await expect(page.getByText("Javascript")).toBeVisible();
+
+})
